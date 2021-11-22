97026ca532b550cc07b0f42e98eba99bebbff643
deejay0825 <plmnj2003@naver.com>
2021-11-22 21:33:22 +0900

    python Bubble sorting

--- Sorting/main.py
+++ Sorting/main.py
@@ -1,1 +1,16 @@
-test
+n = int(input())
+numbers = []
+for _ in range(n):
+  numbers.append(int(input()))
+
+##Bubble Sort
+for i in range(len(numbers)):
+  for j in range(len(numbers)):
+    if numbers[i]<numbers[j]:
+      numbers[i],numbers[j] = numbers[j],numbers[i]
+
+for i in range(len(numbers)):
+  print(numbers[i])
+
+
+##버블 정렬
